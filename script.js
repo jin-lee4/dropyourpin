c1528797b046b7a7f0a39ec0549fec327daf5028
jin <jjin_0627@hotmail.com>
2021-03-06 19:58:01 -0800

    autocomplete search implemented

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-let autocomplete;
-
-function initAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('search_input'),
-  {
-    types: ['establishment'],
-    componentRestrictions: {'country': ['CA']},
-    fields: ['place_id', 'geometry', 'name']
-  })
-
-  autocomplete.addListener('place_changed', onPlaceChanged);
-}
-
-function onPlaceChanged() {
-  var place = autocomplete.getPlace();
-
-  if (!place.geometry) {
-    //user did not select a prediction; reset the input field
-    document.getElementById('autocomplete').placeholder = 'Enter name of location...';
-  } else {
-    //display details about the valid place
-    document.getElementById('details').innerHTML = place.name;
-  }
-}
-
 function reportWindowPopUp() {
   var reportWindow = document.getElementById("reportWindow");
   if (reportWindow.style.display === "none") {
@@ -51,12 +25,47 @@ function initMap() {
   const ubc = { lat: 49.260801, lng: -123.246058 };
   // The map, centered at UBC
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 13,
     center: ubc,
   });
+
+    var autocomplete;
+    autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete'),
+      {
+        types: ['establishment'],
+        componentRestrictions: {'country': ['CA']},
+      });
+
+      autocomplete.addListener('place_changed', function() {
+        fields: ['place_id', 'geometry', 'name', 'formatted_address'];
+        var place = autocomplete.getPlace();
+        var lng = place.geometry.location.lng();
+        var lat = place.geometry.location.lat();
+        var latlng = {lat, lng};
+        var submitReport = document.getElementById("submit");
+        submitReport.addListener("onClick", function() {
+          const marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+          });
+        })
+        console.log(latlng);
+      });
+
   // The marker, positioned at UBC
-  const marker = new google.maps.Marker({
-    position: ubc,
-    map: map,
-  });
+  // const marker = new google.maps.Marker({
+  //   position: ubc,
+  //   map: map,
+  // });
 }
+
+// function initAutocomplete() {
+//     var autocomplete = new google.maps.places.Autocomplete(
+//     document.getElementById('autocomplete'),
+//     {
+//       types: ['establishment'],
+//       componentRestrictions: {'country': ['CA']},
+//       fields: ['place_id', 'geometry', 'name']
+//     });
+// }
